af1db45d52c792da9fcdfca1178d09a1f4044814
shuhi <128314725+Shuhimoon@users.noreply.github.com>
2026-03-03 14:25:36 +0800

    Add files via upload

---
+++ lazyvim/colorscheme.lua
@@ -0,0 +1,22 @@
+-- ~/.config/nvim/lua/plugins/colorscheme.lua
+return {
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("everforest").setup({
+        background = "medium", -- soft/medium/hard
+        transparent_background_level = 1, -- 1 或 2（更透）
+      })
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
